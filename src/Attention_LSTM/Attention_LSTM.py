057ee125859b3ab65eb9a278745a3c09c6e6331c
ninja <1198762933@qq.com>
2018-05-15 14:24:54 +0800

    Prove the effectiveness of dire-refined word vectors on word level

--- src/Attention_LSTM/Attention_LSTM.py
+++ src/Attention_LSTM/Attention_LSTM.py
@@ -4,6 +4,8 @@ from scipy import stats
 
 import input_layer
 
+K = tf.contrib.keras
+
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
@@ -30,56 +32,311 @@ flags.DEFINE_boolean('embedding_normalized', False, '')
 
 
 
-class SSWE_u(object):
+class Attention_LSTM(object):
+    def __init__(self, cl_logits_input_dim=None):
+        self.global_step = tf.train.get_or_create_global_step()
+        self.vocab_freqs = _get_vocab_freqs()
+
+        # Cache VatxtInput objects
+        self.cl_inputs = None
+        self.lm_inputs = None
+
+        # Cache intermediate Tensors that are reused
+        self.tensors = {}
+
+        # Construct layers which are reused in constructing the LM and
+        # Classification graphs. Instantiating them all once here ensures that
+        # variable reuse works correctly.
+        self.layers = {}
+        self.layers['embedding'] = Embedding(
+            FLAGS.vocab_size, FLAGS.embedding_dims, FLAGS.normalize_embeddings,
+            self.vocab_freqs, FLAGS.keep_prob_emb)
+        self.layers['lstm'] = LSTM(
+            FLAGS.rnn_cell_size, FLAGS.rnn_num_layers, FLAGS.keep_prob_lstm_out)
+        self.layers['lm_loss'] = SoftmaxLoss(
+            FLAGS.vocab_size,
+            FLAGS.num_candidate_samples,
+            self.vocab_freqs,
+            name='LM_loss')
+
+        cl_logits_input_dim = cl_logits_input_dim or FLAGS.rnn_cell_size
+        self.layers['cl_logits'] = cl_logits_subgraph(
+            [FLAGS.cl_hidden_size] * FLAGS.cl_num_layers, cl_logits_input_dim,
+            FLAGS.num_classes, FLAGS.keep_prob_cl_hidden)
+
+
+    def classifier_graph(self, inputs):
+        """Constructs classifier graph from inputs to classifier loss.
+
+        * Caches the VatxtInput object in `self.cl_inputs`
+        * Caches tensors: `cl_embedded`, `cl_logits`, `cl_loss`
+
+        Returns:
+            loss: scalar float.
+        """
+        # inputs = _inputs('train', pretrain=False)
+        self.cl_inputs = inputs
+        embedded = self.layers['embedding'](inputs.tokens)
+        self.tensors['cl_embedded'] = embedded
+
+        _, next_state, logits, loss = self.cl_loss_from_embedding(
+                embedded, return_intermediates=True)
+        tf.summary.scalar('classification_loss', loss)
+        self.tensors['cl_logits'] = logits
+        self.tensors['cl_loss'] = loss
+
+        acc = accuracy(logits, inputs.labels, inputs.weights)
+        tf.summary.scalar('accuracy', acc)
+
+        adv_loss = (self.adversarial_loss() * tf.constant(
+                FLAGS.adv_reg_coeff, name='adv_reg_coeff'))
+        tf.summary.scalar('adversarial_loss', adv_loss)
+
+        total_loss = loss + adv_loss
+        tf.summary.scalar('total_classification_loss', total_loss)
+
+        with tf.control_dependencies([inputs.save_state(next_state)]):
+            total_loss = tf.identity(total_loss)
+
+        return total_loss
+
+
+class Embedding(K.layers.Layer):
+    """Embedding layer with frequency-based normalization and dropout."""
+
     def __init__(self,
                  vocab_size,
-                 embedding_size,
-                 window_size,
-                 hidden_size,
-                 vocab_freqs,
-                 embedding_keep_prob=1.0,
-                 linear_keep_prob=1.0,
-                 normalized=False,
-                 loss_mix_weight=0.5):
-        self.layers = {}
-        self.output = {}
+                 embedding_dim,
+                 normalize=False,
+                 vocab_freqs=None,
+                 keep_prob=1.,
+                 **kwargs):
         self.vocab_size = vocab_size
-        self.embedding_size = embedding_size
-        self.normalized = normalized
-        self.vocab_freqs = vocab_freqs
-        self.window_size = window_size
-        self.hidden_size = hidden_size
-        self.embedding_keep_prob = embedding_keep_prob
-        self.linear_keep_prob = linear_keep_prob
-        self.loss_mix_weight = loss_mix_weight
+        self.embedding_dim = embedding_dim
+        self.normalized = normalize
+        self.keep_prob = keep_prob
 
-        emb = tf.Variable(tf.random_uniform(shape=[self.vocab_size, self.embedding_size], minval=-1.0, maxval=1.0), name='embedding')
+        if normalize:
+            assert vocab_freqs is not None
+            self.vocab_freqs = tf.constant(
+                vocab_freqs, dtype=tf.float32, shape=(vocab_size, 1))
+
+        super(Embedding, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        with tf.device('/cpu:0'):
+            self.var = self.add_weight(
+                shape=(self.vocab_size, self.embedding_dim),
+                initializer=tf.random_uniform_initializer(-1., 1.),
+                name='embedding')
+
         if self.normalized:
-            assert self.vocab_freqs is not None
-            emb = self._normalize(emb, self.vocab_size, self.vocab_freqs)
-        self.layers['embedding'] = emb
+            self.var = self._normalize(self.var)
 
-        w_1 = tf.Variable(tf.random_uniform(shape=[self.embedding_size*self.window_size, self.hidden_size], minval=-1.0, maxval=1.0))
-        b_1 = tf.Variable(tf.zeros([self.hidden_size]))
-        self.layers['W_1'] = w_1
-        self.layers['b_1'] = b_1
+        super(Embedding, self).build(input_shape)
 
-        w_2 = tf.Variable(tf.random_uniform(shape=[self.hidden_size, 2], minval=-1.0, maxval=1.0))
-        b_2 = tf.Variable(tf.zeros([2]))
-        self.layers['W_2'] = w_2
-        self.layers['b_2'] = b_2
+    def call(self, x):
+        embedded = tf.nn.embedding_lookup(self.var, x)
+        if self.keep_prob < 1.:
+            shape = embedded.get_shape().as_list()
 
+            embedded = tf.nn.dropout(
+                embedded, self.keep_prob, noise_shape=(shape[0], 1, shape[2]))
+        return embedded
 
-    def _normalize(self, emb, vocab_size, vocab_freqs):
-        self.vocab_freqs = tf.constant(vocab_freqs, dtype=tf.float32, shape=(vocab_size, 1))
+    def _normalize(self, emb):
         weights = self.vocab_freqs / tf.reduce_sum(self.vocab_freqs)
-        mean = tf.reduce_sum(weights * emb, 0, keepdims=True)
-        var = tf.reduce_sum(weights * tf.pow(emb - mean, 2.), 0, keepdims=True)
+        mean = tf.reduce_sum(weights * emb, 0, keep_dims=True)
+        var = tf.reduce_sum(weights * tf.pow(emb - mean, 2.), 0, keep_dims=True)
         stddev = tf.sqrt(1e-6 + var)
         return (emb - mean) / stddev
+    
+    
+class LSTM(object):
+    """LSTM layer using static_rnn.
+
+    Exposes variables in `trainable_weights` property.
+    """
+
+    def __init__(self, cell_size, num_layers=1, keep_prob=1., name='LSTM'):
+        self.cell_size = cell_size
+        self.num_layers = num_layers
+        self.keep_prob = keep_prob
+        self.reuse = None
+        self.trainable_weights = None
+        self.name = name
+
+    def __call__(self, x, initial_state, seq_length):
+        with tf.variable_scope(self.name, reuse=self.reuse) as vs:
+            cell = tf.contrib.rnn.MultiRNNCell([
+                    tf.contrib.rnn.BasicLSTMCell(
+                        self.cell_size,
+                        forget_bias=0.0,
+                        reuse=tf.get_variable_scope().reuse)
+                for _ in range(self.num_layers)
+            ])
+
+            # shape(x) = (batch_size, num_timesteps, embedding_dim)
+            # Convert into a time-major list for static_rnn
+            x = tf.unstack(tf.transpose(x, perm=[1, 0, 2]))
+
+            lstm_out, next_state = tf.contrib.rnn.static_rnn(
+                    cell, x, initial_state=initial_state, sequence_length=seq_length)
+
+            # Merge time and batch dimensions
+            # shape(lstm_out) = timesteps * (batch_size, cell_size)
+            lstm_out = tf.concat(lstm_out, 0)
+            # shape(lstm_out) = (timesteps*batch_size, cell_size)
+
+            if self.keep_prob < 1.:
+                lstm_out = tf.nn.dropout(lstm_out, self.keep_prob)
+
+            if self.reuse is None:
+                self.trainable_weights = vs.global_variables()
+
+        self.reuse = True
+
+        return lstm_out, next_state
 
 
-    # each x is a trigram whose element is also a n-gram
-    # shape(x) = [batch_size, 3]
-    def __call__(self, x, label):
-        embedded = tf.nn.embedding_lookup(self.layers['embedding'], x)
+class SoftmaxLoss(K.layers.Layer):
+    """Softmax xentropy loss with candidate sampling."""
+
+    def __init__(self,
+                 vocab_size,
+                 num_candidate_samples=-1,
+                 vocab_freqs=None,
+                 **kwargs):
+        self.vocab_size = vocab_size
+        self.num_candidate_samples = num_candidate_samples
+        self.vocab_freqs = vocab_freqs
+        super(SoftmaxLoss, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        input_shape = input_shape[0]
+        with tf.device('/cpu:0'):
+            self.lin_w = self.add_weight(
+                shape=(input_shape[-1], self.vocab_size),
+                name='lm_lin_w',
+                initializer=K.initializers.glorot_uniform())
+            self.lin_b = self.add_weight(
+                shape=(self.vocab_size,),
+                name='lm_lin_b',
+                initializer=K.initializers.glorot_uniform())
+
+        super(SoftmaxLoss, self).build(input_shape)
+
+    def call(self, inputs):
+        x, labels, weights = inputs
+        if self.num_candidate_samples > -1:
+            assert self.vocab_freqs is not None
+            labels = tf.expand_dims(labels, -1)
+            sampled = tf.nn.fixed_unigram_candidate_sampler(
+                    true_classes=labels,
+                    num_true=1,
+                    num_sampled=self.num_candidate_samples,
+                    unique=True,
+                    range_max=self.vocab_size,
+                    unigrams=self.vocab_freqs)
+
+            lm_loss = tf.nn.sampled_softmax_loss(
+                    weights=tf.transpose(self.lin_w),
+                    biases=self.lin_b,
+                    labels=labels,
+                    inputs=x,
+                    num_sampled=self.num_candidate_samples,
+                    num_classes=self.vocab_size,
+                    sampled_values=sampled)
+        else:
+            logits = tf.matmul(x, self.lin_w) + self.lin_b
+            lm_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                    logits=logits, labels=labels)
+
+        lm_loss = tf.identity(
+                tf.reduce_sum(lm_loss * weights) / _num_labels(weights),
+                name='lm_xentropy_loss')
+        return lm_loss
+    
+    
+def _num_labels(weights):
+    """Number of 1's in weights. Returns 1. if 0."""
+    num_labels = tf.reduce_sum(weights)
+    num_labels = tf.where(tf.equal(num_labels, 0.), 1., num_labels)
+    return num_labels
+
+
+def cl_logits_subgraph(layer_sizes, input_size, num_classes, keep_prob=1.):
+    """Construct multiple ReLU layers with dropout and a linear layer."""
+    subgraph = K.models.Sequential(name='cl_logits')
+    for i, layer_size in enumerate(layer_sizes):
+        if i == 0:
+            subgraph.add(
+                K.layers.Dense(layer_size, activation='relu', input_dim=input_size))
+        else:
+            subgraph.add(K.layers.Dense(layer_size, activation='relu'))
+
+        if keep_prob < 1.:
+            subgraph.add(K.layers.Dropout(1. - keep_prob))
+    subgraph.add(K.layers.Dense(1 if num_classes == 2 else num_classes))
+    return subgraph
+
+
+def cl_loss_from_embedding(self,
+                           embedded,
+                           inputs=None,
+                           return_intermediates=False):
+      """Compute classification loss from embedding.
+
+      Args:
+          embedded: 3-D float Tensor [batch_size, num_timesteps, embedding_dim]
+          inputs: VatxtInput, defaults to self.cl_inputs.
+          return_intermediates: bool, whether to return intermediate tensors or only
+              the final loss.
+
+      Returns:
+          If return_intermediates is True:
+              lstm_out, next_state, logits, loss
+          Else:
+              loss
+      """
+      if inputs is None:
+          inputs = self.cl_inputs
+
+      lstm_out, next_state = self.layers['lstm'](embedded, inputs.state, inputs.length)
+      logits = self.layers['cl_logits'](lstm_out)
+      loss = classification_loss(logits, inputs.labels, inputs.weights)
+
+      if return_intermediates:
+          return lstm_out, next_state, logits, loss
+      else:
+          return loss
+      
+
+
+def classification_loss(logits, labels, weights):
+    """Computes cross entropy loss between logits and labels.
+
+    Args:
+        logits: 2-D [timesteps*batch_size, m] float tensor, where m=1 if
+            num_classes=2, otherwise m=num_classes.
+        labels: 1-D [timesteps*batch_size] integer tensor.
+        weights: 1-D [timesteps*batch_size] float tensor.
+
+    Returns:
+        Loss scalar of type float.
+    """
+    inner_dim = logits.get_shape().as_list()[-1]
+    with tf.name_scope('classifier_loss'):
+        # Logistic loss
+        if inner_dim == 1:
+            loss = tf.nn.sigmoid_cross_entropy_with_logits(
+                    logits=tf.squeeze(logits), labels=tf.cast(labels, tf.float32))
+        # Softmax loss
+        else:
+            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                    logits=logits, labels=labels)
+
+        num_lab = _num_labels(weights)
+        tf.summary.scalar('num_labels', num_lab)
+        return tf.identity(
+                tf.reduce_sum(weights * loss) / num_lab, name='classification_xentropy')
---
+++ src/Refine/senti_refine.py
@@ -0,0 +1,150 @@
+from src.test import we
+from sklearn.decomposition import PCA
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+
+
+def save(words, vecs, filename):
+    with open(filename, "w") as f:
+        f.write("\n".join([w + " " + " ".join([str(x) for x in v]) for w, v in zip(words, vecs)]))
+    print("Wrote to", filename)
+
+
+def get_sentiwords():
+    path = 'D://Codes/NSE/data/raw/SentiWords/SentiWords_1.1.txt'
+    coll = []
+    with open(path) as f:
+        content = [line for line in f.readlines() if line[0] != '#']
+        for line in content:
+            pair = line.split()
+            score = float(pair[1])
+            pair = pair[0].split('#')
+            pair.append(score)
+            coll.append(pair)
+    return coll
+
+wordpair = [line.strip().split() for line in open('D://Codes/NSE/data/used/seeds/wordpairs-greater-than-0.5').readlines()]
+sub_words_p = [line[0] for line in wordpair]
+sub_words_n = [line[1] for line in wordpair]
+
+
+
+def find_senti_direction(topK=1):
+    picked = we.WordEmbedding('D://Codes/NSE/data/used/embeddings/word-picked.vec')
+
+    definitional = []
+
+    for index in range(len(sub_words_p)):
+        wp = sub_words_p[index]
+        wn = sub_words_n[index]
+        if wp not in picked.index or wn not in picked.words:
+            print(wp, wn, 'not in word-picked.vec')
+            continue
+        definitional.append([wp, wn])
+
+    pca = we.doPCA(definitional, picked)
+    senti_direction = pca.components_[0]
+    print(pca.explained_variance_ratio_)
+    # dire_0 = picked.vecs[picked.index[definitional[4][0]]] - picked.vecs[picked.index[definitional[4][1]]]
+    # dire_0 /= np.linalg.norm(dire_0)
+    return senti_direction
+
+
+def refine_word(lexicon, direction, alpha=0.5):
+    picked = we.WordEmbedding('D://Codes/NSE/data/used/embeddings/word-picked.vec')
+
+    words = {}
+    vecs = {}
+    for word, _, score in lexicon:
+        if word not in picked.words:
+            continue
+        elif word in words and abs(score) < abs(words[word]):
+            continue
+        else:
+            words[word] = score
+            vec = picked.vecs[picked.index[word]]
+            vec_senti = np.sum(np.multiply(vec, direction)) / (np.linalg.norm(vec) * np.linalg.norm(direction)) * direction
+            vec_vertical_to_senti = vec - vec_senti
+            # print(np.linalg.norm(vec_senti))
+            # print(np.linalg.norm(vec_vertical_to_senti))
+            new_vec_senti = direction * score
+            new_vec = vec_vertical_to_senti + alpha * new_vec_senti
+            new_vec = new_vec / np.linalg.norm(new_vec)
+            vecs[word] = new_vec
+    save(words.keys(), vecs.values(), 'D://Codes/NSE/data/used/embeddings/refined-word-picked.vec')
+
+
+
+def get_simlex999():
+    vocab_filename = 'D://Codes/NSE/data/used/embeddings/word-picked'
+    def make_vocab_ids(vocab_filename):
+        with open(vocab_filename) as vocab_f:
+            return dict([(line.strip(), i) for i, line in enumerate(vocab_f)])
+    path = 'D://Codes/NSE/data/raw/embeddingTest/simlex999.txt'
+    with open(path) as f:
+        evals = np.array(pd.read_csv(f, comment='#', sep='\t'))
+        vocab = make_vocab_ids(vocab_filename)
+        evals = [pair for pair in evals if pair[0] in vocab and pair[1] in vocab]
+        print('Get %s pairs from simlex-999' % np.shape(evals)[0])
+        return np.array(evals)
+
+
+def get_wordsim353():
+    vocab_filename = 'D://Codes/NSE/data/used/embeddings/word-picked'
+    def make_vocab_ids(vocab_filename):
+        with open(vocab_filename) as vocab_f:
+            return dict([(line.strip(), i) for i, line in enumerate(vocab_f)])
+    path = 'D://Codes/NSE/data/raw/embeddingTest/wordsim353.tsv'
+    with open(path) as f:
+        evals = np.array(pd.read_csv(f, comment='#', sep='\t'))
+        vocab = make_vocab_ids(vocab_filename)
+        evals = [pair for pair in evals if pair[0] in vocab and pair[1] in vocab]
+        print('Get %s pairs from wordsim-353' % np.shape(evals)[0])
+        return np.array(evals)
+
+
+def get_simverb3500():
+    vocab_filename = 'D://Codes/NSE/data/used/embeddings/word-picked'
+    def make_vocab_ids(vocab_filename):
+        with open(vocab_filename) as vocab_f:
+            return dict([(line.strip(), i) for i, line in enumerate(vocab_f)])
+    path = 'D://Codes/NSE/data/raw/embeddingTest/simverb-3500-data/SimVerb-3500.txt'
+    with open(path) as f:
+        evals = f.readlines()
+        evals = [i.split() for i in evals]
+        evals = [[i[0], i[1], np.float32(i[3])] for i in evals]
+        vocab = make_vocab_ids(vocab_filename)
+        evals = [pair for pair in evals if pair[0] in vocab and pair[1] in vocab]
+        print('Get %s pairs from SimVerb-3500' % np.shape(evals)[0])
+        return evals
+
+
+# lexicon = get_sentiwords()
+# dire = find_senti_direction(1)
+# refine_word(lexicon, dire)
+
+def eval_w2v_wordsim(simlex):
+    refined = we.WordEmbedding('D://Codes/NSE/data/used/embeddings/word-picked.vec')
+    results = []
+
+    for pair in simlex:
+        w1 = pair[0]
+        w2 = pair[1]
+        if w1 not in refined.words or w2 not in refined.words:
+            print('Cant find', w1, w2)
+            continue
+
+        w1v = refined.vecs[refined.index[w1]]
+        w2v = refined.vecs[refined.index[w2]]
+        results.append([w1, w2, pair[2], np.sum(np.multiply(w1v, w2v))])
+
+    print(stats.pearsonr([i[2] for i in results], [i[3] for i in results])[0], stats.spearmanr([i[2] for i in results], [i[3] for i in results]).correlation)
+
+    # for i in sorted(results, key=lambda x: x[3]):
+    #     print(i)
+
+
+
+eval_w2v_wordsim(get_simverb3500())
